815db06284c779f1d641478fad09630cef59687c
Brandon Bruno <bmbruno@gmail.com>
2025-05-09 13:08:17 -0400

    Implemented modal open/close logic.

--- src/scripts/default.js
+++ src/scripts/default.js
@@ -6,7 +6,49 @@
 
             console.log("EZD init started...");
 
+            EZDemo.wireUI();
+
             console.log("EZD init complete.");
+
+        },
+
+        // Add event handlers and other setup for app-wide common UI elements
+        wireUI: () => {
+
+            // Modal close button
+            let modalClose = document.querySelectorAll("a.close-modal");
+            modalClose.forEach((element) => { element.addEventListener("click", () => EZDemo.handleModalCloseClick(element) ) });
+
+        },
+
+        openModal: (id) => {
+
+            let modal = document.getElementById(id);
+            if (!modal)
+                return;
+
+            // TODO: open model
+            modal.style.display = "block";
+
+        },
+
+        closeModal: (id) => {
+
+            let modal = document.getElementById(id);
+            if (!modal)
+                return;
+
+            modal.style.display = "none";
+
+        },
+
+        handleModalCloseClick: (e) => {
+
+            if (!e)
+                return;
+
+            let modalID = e.getAttribute("data-id");
+            EZDemo.closeModal(modalID);
 
         }
 
